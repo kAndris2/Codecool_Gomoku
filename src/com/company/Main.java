88ab10b7f891d985f3aae54be8d194362535ac85
burgonyapure <deejay.spory@gmail.com>
2020-06-22 10:22:03 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner io = new Scanner(System.in);
+
+
+        System.out.println("ayy");
+    }
+}
